66f1e0e7d9256f2139ab4346e0164c8773a89875
Asriel Camora <asriel@camora.dev>
2024-10-03 08:39:23 -0700

    Remove patches folder

--- patches/relations.ps1
+++
@@ -1,6 +0,0 @@
-cd ..
-Get-ChildItem -Directory Schemas | ForEach-Object {
-    $dir = $_
-    Get-ChildItem -File patches/*.patch | ForEach-Object { git apply "patches/$($_.Name)" --directory "Schemas/$($dir.Name)" }
-}
-cd patches
